4d7ef1e269caa9354e6b77b6ac2d54ef68e03739
Dominick Peluso <peluso.dominick@gmail.com>
2016-09-08 16:11:49 -0400

    Complete get size

--- src/InjectLite.js
+++ src/InjectLite.js
@@ -1,6 +1,8 @@
+// For each helper function
 var forEach = function(array, callback){
    var currentValue, index;
-   for (i = 0; i < array.length; i += 1) {
+   var i = 0;
+   for (i; i < array.length; i += 1) {
       if(typeof array[i] == "undefined"){
          currentValue = null;
       } else {
@@ -34,7 +36,10 @@ var getElementProperties = function(s : Switch ){
 		extension: s.getPropertyValue("Extension"),
 		inject_type: s.getPropertyValue("InjectType"),
 		target_url: null,
-		target_basename: null
+		target_basename: null,
+		count_key: s.getPropertyValue("CountPrivateDataKey"),
+		size_key: s.getPropertyValue("SizePrivateDataKey"),
+		size_unit: s.getPropertyValue("SizeUnit")
 	};
 
 	// Determine the job path based on prop
@@ -118,6 +123,94 @@ var handleInject = function( prop, s : Switch, job : Job, callback ){
 			}
 		}
 
+	} else if(prop.inject_type == "Count files"){
+		type = getTargetType(prop.target_url);
+
+		if(type === false){
+			callback(false, "Target file does not exist!", job.getPath());
+		} else if(type === "file"){
+			job.setPrivateData(prop.count_key, 1);
+			callback(true, "Target job is a single file.", job.getPath());
+		} else if(type === "dir"){
+
+			// Function for recursively counting files within a directory
+			get_recursive_file_count = function(target_dir, entry_list){
+				file_count = 0;
+				forEach(entry_list, function(file_name, index){
+					type = getTargetType(target_dir + file_name);
+					if(type === "dir"){
+						sub_target_dir = target_dir + file_name + getDirectorySeperator(s);
+						sub_dir = new Dir(sub_target_dir);
+						sub_file_list = sub_dir.entryList("*", Dir.Files|Dir.Dirs|Dir.NoDotAndDotDot, Dir.Name);
+						result_count = get_recursive_file_count(sub_target_dir, sub_file_list);
+					} else if(type === "file"){
+						file_count++;
+					}
+				});
+				return file_count;
+			}
+
+			dir = new Dir( prop.target_url );
+			file_list = dir.entryList("*", Dir.Files|Dir.Dirs|Dir.NoDotAndDotDot, Dir.Name);
+			count = get_recursive_file_count(prop.target_url, file_list);
+
+			job.setPrivateData(prop.count_key, count);
+			callback(true, "Target job contains " + count + " files.", job.getPath());
+		}
+
+	} else if(prop.inject_type == "Get size"){
+		type = getTargetType(prop.target_url);
+
+		if(type === false){
+			callback(false, "Target file does not exist!", job.getPath());
+		} else if(type === "file"){
+
+			fs = new FileStatistics(prop.target_url);
+			byte_count = fs.getByteCount();
+
+			job.setPrivateData(prop.size_key, byte_count);
+			callback(true, "Target file is " + byte_count + " bytes.", job.getPath());
+
+		} else if(type === "dir"){
+
+			// Function for recursively counting bytes within a directory
+			get_recursive_byte_count = function(target_dir, entry_list){
+				byte_count = 0;
+				forEach(entry_list, function(file_name, index){
+					type = getTargetType(target_dir + file_name);
+					if(type === "dir"){
+						sub_target_dir = target_dir + file_name + getDirectorySeperator(s);
+						sub_dir = new Dir(sub_target_dir);
+						sub_file_list = sub_dir.entryList("*", Dir.Files|Dir.Dirs|Dir.NoDotAndDotDot, Dir.Name);
+						result_count = get_recursive_byte_count(sub_target_dir, sub_file_list);
+					} else if(type === "file"){
+						fs = new FileStatistics(target_dir + file_name);
+						byte_count += fs.getByteCount();
+					}
+				});
+				return byte_count;
+			}
+
+			// Function to convert bytes to other sizes
+			bytesToSize = function(bytes, unit) {
+				if(bytes === 0 || unit == "Bytes") return bytes;
+				units = {};
+				units.KB = 1024;
+				units.MB = units.KB * 1024;
+				units.GB = units.MB * 1024;
+				units.TB = units.GB * 1024;
+				return Math.floor(bytes / units[unit]);
+			};
+
+			dir = new Dir( prop.target_url );
+			file_list = dir.entryList("*", Dir.Files|Dir.Dirs|Dir.NoDotAndDotDot, Dir.Name);
+			byte_count = get_recursive_byte_count(prop.target_url, file_list);
+
+			converted_size = bytesToSize(byte_count, prop.size_unit);
+
+			job.setPrivateData(prop.size_key, converted_size);
+			callback(true, "Target directory is " + converted_size + " " + prop.size_unit + ".", job.getPath());
+		}
 	}
 }
 
